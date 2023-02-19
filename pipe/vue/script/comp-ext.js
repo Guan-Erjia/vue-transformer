8e60b93f3db2d4788ee08e37375b5827484b455b
guanjianzhou <1357023518@qq.com>
2023-02-19 12:16:33 +0800

    feat: 加注释

--- pipe/vue/script/comp-ext.js
+++ pipe/vue/script/comp-ext.js
@@ -8,17 +8,17 @@ class Bundle {
 export default (script, item) => {
   const componentsString = getOptionString(script, 'components')
   if (componentsString?.trim()) {
-    item.value = item.value.replace(componentsString, '') //字符串获取完毕，先干掉过滤器
-    let components = trimOption(componentsString).split(',')
-    // console.log(components.map(item => item.trim()))
+    item.value = item.value.replace(componentsString, '')
+    let components = trimOption(componentsString).split(',').map(item => item.trim())
+    // console.log(components)
     const imports = script.match(/import\s.+\sfrom[\s]+['|"].+['|"]/g)
     if (imports.length) {
-      const vueImports = imports.filter(each => {
+      const vueImports = imports.filter(each => { //过滤有效组件引入
         return components.some(each1 => each.includes(each1))
       }).filter(each => {
-        return !(each.endsWith('.vue"') || each.endsWith(".vue'"))
+        return !(each.endsWith('.vue"') || each.endsWith(".vue'")) //过滤需要替换的引入字符串
       })
-      const bundles = vueImports.map(vueImport => {
+      const bundles = vueImports.map(vueImport => { //引号判断
         return new Bundle(vueImport,
           vueImport.endsWith('"') ?
             vueImport.slice(0, vueImport.length - 1) + '.vue"' : vueImport.slice(0, vueImport.length - 1) + ".vue'")
@@ -26,7 +26,7 @@ export default (script, item) => {
       bundles.forEach(each => {
         item.value = item.value.replaceAll(each.pre, each.rep)
       })
-      console.log(bundles)
+      // console.log(bundles)
     }
   }
 }
