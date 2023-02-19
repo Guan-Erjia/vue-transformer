5e2f6d4281da6487f9b098bb6569f5fbd361f5ba
guanjianzhou <1357023518@qq.com>
2023-02-19 23:43:01 +0800

    fix: v-model处理修改

--- pipe/vue/script/v-model.js
+++ pipe/vue/script/v-model.js
@@ -2,13 +2,13 @@ import { getOptionString } from "../../../utils/index.js";
 export default (script, item) => {
   const props = getOptionString(script, 'props')
   if (props) {
-    const valueProp = props.match(/value\s*:\s*.*?,/g)
+    const valueProp = props.match(/[\s|\S]+value\s*:\s*.*?,/g)
     if (valueProp) {
       valueProp.forEach(each => {
         const rep = each.replace('value', 'modelValue')
         item.value = item.value.replace(each, rep)
       })
-      console.log(item.value)
+      console.log(valueProp)
     }
   }
 }
