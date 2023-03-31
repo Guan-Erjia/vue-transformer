ef8e36e0859c6a6a2731f7c86b72f3c4afd6509f
guanjianzhou <1357023518@qq.com>
2023-04-01 01:37:02 +0800

    feat: for循环自己解决吧

--- pipe/vue/template/v-for.js
+++ pipe/vue/template/v-for.js
@@ -3,6 +3,13 @@ import parseHTML from '../../../utils/parse-html.js'
 export default (template, item) => {
   const target = template.match(/<.+v-for.+?>/gm)
   if (target) {
-    target.forEach(each => parseHTML(template, each))
+    target.forEach(each => {
+      const target1 = each.match(/<.+v-if.+?>/gm)
+      if (target1) {
+        target1.forEach(each1 => {
+          item.value = item.value.replace(each1, each1.replace('v-for', 'DIRECTIVE_NEED_FOR_CHANGE'))
+        })
+      }
+    })
   }
 }
